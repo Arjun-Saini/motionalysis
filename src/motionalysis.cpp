b1e8276d0f8e3dce26dd8ee752684c22f7130e66
Arjun <arjun.saini@gmail.com>
2021-05-25 22:05:00 -0500

    new project

---
+++ src/motionalysis.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "Particle.h"
+#line 1 "e:/IoT/motionalysis/src/motionalysis.ino"
+/*
+ * Project motionalysis
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "e:/IoT/motionalysis/src/motionalysis.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}

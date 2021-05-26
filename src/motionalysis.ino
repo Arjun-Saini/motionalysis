4a5351cec589db92bd9708ea69bbf7473e2f884f
Arjun <arjun.saini@gmail.com>
2021-05-26 00:04:55 -0500

    convert position to acceleration

--- src/motionalysis.ino
+++ src/motionalysis.ino
@@ -1,21 +1,34 @@
 SYSTEM_MODE(MANUAL);
 
 #include "Adafruit_LIS3DH.h"
-
+#include "Adafruit_Sensor.h"
 
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
 void setup() {
-  Serial.begin(9600);
   Wire.begin();
+  Serial.begin(9600);
   lis.begin(0x18);
 }
 
+sensors_event_t event1;
+
 void loop() {
   lis.read();
-  Serial.print("X:  "); Serial.print(lis.x);
-  Serial.print("  \tY:  "); Serial.print(lis.y);
-  Serial.print("  \tZ:  "); Serial.print(lis.z);
+
+  Serial.print("X:  "); Serial.print(lis.x_g);
+  Serial.print("  \tY:  "); Serial.print(lis.y_g);
+  Serial.print("  \tZ:  "); Serial.print(lis.z_g);
+
+  //sensors_event_t event;
+  //lis.getEvent(&event);
+
+  //Serial.print("\t\tX: "); Serial.print(event.acceleration.x);
+  //Serial.print(" \tY: "); Serial.print(event.acceleration.y);
+  //Serial.print(" \tZ: "); Serial.print(event.acceleration.z);
+  //Serial.println(" m/s^2 ");
+
   Serial.println();
+
   delay(200);
 }

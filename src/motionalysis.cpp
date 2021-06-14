4743b4910250aa3ecd15c94857fc42d10b5438ad
Arjun-Saini <arjun.saini@gmail.com>
2021-06-14 01:31:40 -0500

    reduce time of wifi connection

--- src/motionalysis.cpp
+++ src/motionalysis.cpp
@@ -3,21 +3,28 @@
 /******************************************************/
 
 #include "Particle.h"
-#line 1 "/Users/trylaarsdam/Documents/dev/motionalysis/src/motionalysis.ino"
+#line 1 "e:/IoT/motionalysis/src/motionalysis.ino"
+void setup();
+void loop();
+void callback(char* topic, byte* payload, unsigned int length);
+#line 1 "e:/IoT/motionalysis/src/motionalysis.ino"
+SYSTEM_MODE(MANUAL)
+
 #include "Adafruit_LIS3DH.h"
 #include "MQTT.h"
 #include <string>
 
-void setup();
-void loop();
-void callback(char* topic, byte* payload, unsigned int length);
-#line 5 "/Users/trylaarsdam/Documents/dev/motionalysis/src/motionalysis.ino"
-String payload;
+#define I2C_ADDRESS 0x18
+#define DELAY 200 //pause between data readings in milliseconds
+#define AWAKE_DURATION 5000 //how long argon will be awake for in milliseconds after initial movement
+#define CLICK_THRESHHOLD 60 //higher is less sensitive
+#define GRAVITY 9.8066
+#define INTERRUPT_PIN D5 //pin on argon that is connected to interrupt pin on accelerometer
+#define MQTT_DELAY 100 //milliseconds between each publish of mqtt data, can't be too low or else mqtt won't be able to keep up
+
+String payload[AWAKE_DURATION / DELAY + 1];
+int timeLeft;
 int counter;
-const int DELAY = 200; //pause between data readings in milliseconds
-const int AWAKE_DURATION = 5000; //how long argon will be awake for in milliseconds after initial movement
-const int CLICK_THRESHHOLD = 60; //higher is less sensitive
-sensors_event_t event;
 
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 MQTT client("lab.thewcl.com", 1883, callback);
@@ -27,40 +34,41 @@ void setup() {
   Serial.begin(9600);
 
   //start transmission from accelerometer
-  lis.begin(0x18);
+  lis.begin(I2C_ADDRESS);
   lis.setRange(LIS3DH_RANGE_2_G);
   lis.setClick(1, CLICK_THRESHHOLD);
 
-  //connect to MQTT
-  client.connect(System.deviceID());
+  pinMode(INTERRUPT_PIN, INPUT);
+  config.mode(SystemSleepMode::HIBERNATE).gpio(INTERRUPT_PIN, RISING);
 
-  pinMode(D5, INPUT);
-  config.mode(SystemSleepMode::ULTRA_LOW_POWER).gpio(D5, RISING);
-
-  counter = AWAKE_DURATION + DELAY;
+  timeLeft = AWAKE_DURATION + DELAY;
 }
 
 void loop() {
-  counter -= DELAY;
-  Serial.println(counter);
-  if(counter <= 0){
+  timeLeft -= DELAY;
+  if(timeLeft <= 0){
+    WiFi.on();
+    WiFi.connect();
+    while(!WiFi.ready()){
+
+    }
+    //connect and publish to MQTT
+    client.connect(System.deviceID());
+    for(int i = 0; i < AWAKE_DURATION / DELAY + 1; i++){
+      client.publish("test/motionalysis", payload[i]);
+      Serial.println(payload[i]);
+      client.loop();
+      delay(MQTT_DELAY);
+    }
+
     counter = AWAKE_DURATION;
     System.sleep(config);
   }
 
   //read data from accelerometer
   lis.read();
-  payload = "X: " + String(9.8066 * lis.x_g) + "\tY: " + String(9.8066 * lis.y_g) + "\tZ: " + String(9.8066 * lis.z_g);
-
-  //reconnect to mqtt if necessary
-  if(client.isConnected()){
-    client.loop();
-  } else{
-    client.connect(System.deviceID());
-  }
-
-  //publish to mqtt
-  client.publish("test/motionalysis", payload);
+  payload[counter] = "X: " + String(GRAVITY * lis.x_g) + "\tY: " + String(GRAVITY * lis.y_g) + "\tZ: " + String(GRAVITY * lis.z_g);
+  counter++;
 
   //pause between each loop to slow rate of data gathering
   delay(DELAY);

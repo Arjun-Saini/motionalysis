81a1d2acf447f6e9b78ffc97d71ee22cc160f056
Arjun-Saini <arjun.saini@gmail.com>
2021-08-11 10:34:44 -0500

    comments and ota

--- src/motionalysis.ino
+++ src/motionalysis.ino
@@ -1,37 +1,29 @@
 SYSTEM_MODE(MANUAL)
+PRODUCT_ID(15161)
+PRODUCT_VERSION(1)
 
 #include "Adafruit_LIS3DH.h"
 #include "HttpClient/HttpClient.h"
 #include <string>
 
-#define I2C_ADDRESS 0x19
-#define GRAVITY 9.8066
-#define SDO_OUTPUT_PIN D8
-#define CONFIG_WAIT_TIME 30000
+#define I2C_ADDRESS 0x19 //i2c address for lis3dh accelerometer
+#define SDO_OUTPUT_PIN D8 //reduces power usage on lis3dh
+#define CONFIG_WAIT_TIME 30000 //time in ms after power up where device waits for user input, if none is detected it will start collecting data
 #define DEFAULT_SLEEP_DURATION 1000
 #define DEFAULT_WIFI_INTERVAL 60000
 #define SLEEP_DELAY 70
-#define WIFI_TEST_TIMEOUT 30000
-#define ROUNDING_FACTOR 10
-#define SENSITIVITY_TOLERANCE 0.5
+#define WIFI_TEST_TIMEOUT 30000 //time in ms after wifi test before timing out
+#define SENSITIVITY_TOLERANCE 0.5 //minimum change in g value for new data to be sent
 
 int sleepDuration = DEFAULT_SLEEP_DURATION;
 int wifiInterval = DEFAULT_WIFI_INTERVAL;
 
-String payload = "";
-String prevPayload;
+String payload, prevPayload = "";
 bool valuesChanged = false;
 int wifiTimeLeft;
-float x, y, z;
 String unixTime;
-int isMoving;
 String ssid, password = "";
 
-// float *currentArr;
-// float *previousArr;
-
-// float currentArr[3 * (60 + 1)];
-// float previousArr[3 * (60 + 1)];
 Vector<float> previousArr(3 * (3600 + 1));
 
 Adafruit_LIS3DH lis = Adafruit_LIS3DH();
@@ -77,10 +69,9 @@ bool timeFix = false;
 void setup() {
   System.updatesEnabled();
   EEPROM.get(200, wifiTimeLeft);
-  config.mode(SystemSleepMode::ULTRA_LOW_POWER).duration(1000 - SLEEP_DELAY);
 
-  // currentArr = new float[arrSizeBuffer];
-  // previousArr = new float[arrSizeBuffer];
+  //sets up default config for sleep
+  config.mode(SystemSleepMode::ULTRA_LOW_POWER).duration(1000 - SLEEP_DELAY);
 
   Serial.begin(9600);
   
@@ -121,6 +112,8 @@ void setup() {
 
 void loop() {
   Particle.process();
+
+  //connects to particle cloud if ota command entered through ble
   if(ota){
     while(!Particle.connected()){
       Particle.process();
@@ -131,7 +124,9 @@ void loop() {
   }
 
   time2 = millis();
+
   if(bleInput | ((time2 - CONFIG_WAIT_TIME >= time1) && WiFi.hasCredentials() && !(BLE.connected()))){
+    //connects to particle cloud once to synchronize the time
     if(!timeFix){
       Particle.process();
       WiFi.on();
@@ -146,36 +141,21 @@ void loop() {
       timeFix = true;
     }
 
+    //get values stored in eeprom
     EEPROM.get(0, dsid);
     EEPROM.get(100, sleepDuration);
     EEPROM.get(200, wifiInterval);
     config.mode(SystemSleepMode::ULTRA_LOW_POWER).duration(sleepDuration - SLEEP_DELAY);
 
+    //get data from accelerometer
     lis.read();
     unixTime = Time.now();
-    // isMoving = 0;
-
-    // if(lis.x_g >= 0.8 && lis.x_g <= 1.2){
-    //   x = GRAVITY * (lis.x_g - 1);
-    //   y = GRAVITY * lis.y_g;
-    //   z = GRAVITY * lis.z_g;
-    // } else if(lis.y_g >= 0.8 && lis.y_g <= 1.2){
-    //   x = GRAVITY * lis.x_g;
-    //   y = GRAVITY * (lis.y_g - 1);
-    //   z = GRAVITY * lis.z_g;
-    // }else if(lis.z_g >= 0.8 && lis.z_g <= 1.2){
-    //   x = GRAVITY * lis.x_g;
-    //   y = GRAVITY * lis.y_g;
-    //   z = GRAVITY * (lis.z_g - 1);
-    // }
+    
     Serial.println(lis.x_g);
     Serial.println(lis.y_g);
     Serial.println(lis.z_g);
-
-    // if(abs(x) > 1 || abs(y) > 1 || abs(z) > 1){
-    //   isMoving = 1;
-    // }
     
+    //determines if the new data has changed enough for different values to be sent
     if(abs(lis.x_g - previousArr.at(size)) > SENSITIVITY_TOLERANCE || abs(lis.y_g - previousArr.at(size + 1)) > SENSITIVITY_TOLERANCE || abs(lis.z_g - previousArr.at(size + 2)) > SENSITIVITY_TOLERANCE){
       valuesChanged = true;
       previousArr.insert(size, lis.x_g);
@@ -195,14 +175,15 @@ void loop() {
     Serial.println(wifiInterval);
     Serial.println(t3);
 
-    //lis.setupLowPowerWakeMode(16);
-
+    //automatically disconnects from ble if it is still connected before sleep, this step is necessary or else it causes the device to crash
     BLE.disconnect();
     BLE.off();
+
     System.sleep(config);
     t2 = millis();
     t3 = t2 - t1;
     
+    //connects to wifi and publishes data
     if(wifiTimeLeft <= 0){
       WiFi.on();
       WiFi.connect();
@@ -214,6 +195,7 @@ void loop() {
       Serial.println(Particle.timeSyncedLast());
       Serial.println(Time.now());
 
+      //formatting payload into json format, sets the correct payload as the http request body
       payload.remove(payload.length() - 1);
       prevPayload.remove(prevPayload.length() - 1);
       if(valuesChanged){
@@ -224,15 +206,16 @@ void loop() {
         request.body = "{\"data\":[" + prevPayload + "]}";
         Serial.println("Values not changed, same array sent");
       }
-      size = 0;
-      //request.body = dsid + "," + payload;
 
+      //sends post request to server
       http.post(request, response, headers);
       Serial.println("Status: " + response.status);
       Serial.println("Body: " + response.body);
 
+      //reset variables
       payload = "";
       prevPayload = "";
+      size = 0;
       
       wifiTimeLeft = wifiInterval;
 
@@ -240,15 +223,20 @@ void loop() {
       WiFi.off();
     }
 
+    //countdown to wifi publish
     wifiTimeLeft -= sleepDuration;
   }
 }
 
+//ble interface
 void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, void* context){
   Serial.println(len);
   inputBuffer = "";
+
+  //each case is a separate prompt
   switch(count){
     case 0:{
+      //ssid prompt
       SSID:
       txCharacteristic.setValue("\nCredentials are currently stored for:\n[");
       networkCount = WiFi.getCredentials(networks, 5);
@@ -265,6 +253,7 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
       break;
     }
     case 1:{
+      //store ssid
       for(int i = 0; i < len - 1; i++){
         inputBuffer += (char)data[i];
         ssid = inputBuffer;
@@ -273,6 +262,7 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
       Serial.println(ssid);
       Serial.println(ssid.length());
       if(ssid == ""){
+        //dsid prompt if wifi prompt skipped
         count = 3;
         EEPROM.get(0, dsid);
         txCharacteristic.setValue("\nCurrent DSID is [");
@@ -281,15 +271,18 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
         }
         txCharacteristic.setValue("]\nEnter device DSID (blank to skip): ");
       }else if(ssid == "clear"){
+        //go back to ssid prompt if credentials are cleared
         WiFi.clearCredentials();
         count = 0;
         goto SSID;
       }else{
+        //password prompt if ssid entered
         txCharacteristic.setValue("\nEnter network password: ");
       }
       break;
     }
     case 2:{
+      //save credentials with given ssid and password
       for(int i = 0; i < len - 1; i++){
         Serial.println(data[i]);
         inputBuffer += (char)data[i];
@@ -299,6 +292,8 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
       Serial.println(password.length());
       WiFi.setCredentials(ssid, password);
       Serial.println("\n\nCredentials set with ssid: " + ssid + "\npassword: " + password + "\n\n");
+
+      //wifi test prompt
       txCharacteristic.setValue("\nEnter 'test' to test credentials (blank to skip): ");
       break;
     }
@@ -308,6 +303,7 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
         inputBuffer += (char)data[i];
       }
       if(inputBuffer == "test" && WiFi.hasCredentials()){
+        //attempt to connect with credentials, times out if unsuccessful
         t4 = millis();
         WiFi.on();
         WiFi.connect();
@@ -324,12 +320,15 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
         if(wifiTest){
           txCharacteristic.setValue("Success!\n");
         }else{
+          //go back to ssid prompt if test failed
           txCharacteristic.setValue("ERROR: WiFi connection timeout\n");
           count = 0;
           wifiTest = true;
           goto SSID;
         }
       }
+
+      //dsid prompt
       EEPROM.get(0, dsid);
       txCharacteristic.setValue("\nCurrent DSID is [");
       if(dsid != -1){
@@ -339,6 +338,7 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
       break;
     }
     case 4:{
+      //store dsid in eeprom
       for(int i = 0; i < len - 1; i++){
         Serial.println(data[i]);
         inputBuffer += (char)data[i];
@@ -351,6 +351,8 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
       EEPROM.get(0, dsid);
       Serial.println("dsid: " + dsid);
       EEPROM.get(100, sleepDuration);
+
+      //prompt for data collection interval
       txCharacteristic.setValue("\nCurrent value for data collection interval is [");
       if(sleepDuration != -1){
         txCharacteristic.setValue(String(sleepDuration));
@@ -359,6 +361,7 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
       break;
     }
     case 5:{
+      //store data collection interval in eeprom
       for(int i = 0; i < len - 1; i++){
         Serial.println(data[i]);
         inputBuffer += (char)data[i];
@@ -372,6 +375,8 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
       config.mode(SystemSleepMode::ULTRA_LOW_POWER).duration(sleepDuration - SLEEP_DELAY);
       Serial.println(sleepDuration);
       EEPROM.get(200, wifiInterval);
+
+      //prompt for wifi connection interval
       txCharacteristic.setValue("\nCurrent value for WiFi connection interval is [");
       if(wifiInterval != -1){
         txCharacteristic.setValue(String(wifiInterval / 1000));
@@ -380,6 +385,7 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
       break;
     }
     case 6:{
+      //store wifi connection interval in eeprom
       for(int i = 0; i < len - 1; i++){
         Serial.println(data[i]);
         inputBuffer += (char)data[i];
@@ -392,10 +398,13 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
       EEPROM.get(200, wifiInterval);
       wifiTimeLeft = wifiInterval;
       Serial.println(wifiInterval);
+
+      //prompt for ota
       txCharacteristic.setValue("\nEnter 'ota' to wait for OTA update (blank to skip): ");
       break;
     }
     case 7:{
+      //enter ota mode if command entered
       for(int i = 0; i < len - 1; i++){
         Serial.println(data[i]);
         inputBuffer += (char)data[i];
@@ -404,6 +413,8 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
         System.updatesEnabled();
         ota = true;
       }
+
+      //causes data collection to begin
       bleInput = true;
       digitalWrite(D7, LOW);
     }
@@ -412,12 +423,14 @@ void onDataReceived(const uint8_t* data, size_t len, const BlePeerDevice& peer, 
   count++;
 }
 
+//d7 led turns on when ble connected
 void connectCallback(const BlePeerDevice& peer, void* context){
   count = 0;
   Serial.println("connected");
   digitalWrite(D7, HIGH);
 }
 
+//d7 led turns off when ble disconnected
 void disconnectCallback(const BlePeerDevice& peer, void* context){
   Serial.println("disconnected");
   digitalWrite(D7, LOW);

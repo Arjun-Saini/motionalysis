1a966cb6b1adb925803fa2ef567035ff460d2946
Todd Rylaarsdam <todd@toddr.org>
2022-02-01 08:52:40 -0600

    sync time from API endpoint

--- src/http.hpp
+++ src/http.hpp
@@ -11,9 +11,13 @@ http_header_t headers[] = {
   {"Content-Type", "application/json"},
   {NULL, NULL}
 };
+http_header_t timeheaders[] = {
+  {NULL, NULL}
+};
 http_request_t request;
 http_response_t response;
-
+http_request_t timerequest;
+http_response_t timeresponse;
 
 int networkCount;
 WiFiAccessPoint networks[5];
@@ -23,6 +27,35 @@ void HTTPRequestSetup() {
   request.hostname = kHTTPHostname;
   request.port = kHTTPRequestPort;
   request.path = "/";
+
+  timerequest.hostname = kHTTPHostname;
+  timerequest.port = kHTTPRequestPort;
+  timerequest.path = "/time";
+}
+
+void getTime() {
+  WiFi.on();
+  WiFi.connect();
+  while(!WiFi.ready()){
+    delay(100);
+  }
+  if(WiFi.ready() != true) {
+    WITH_LOCK(Serial) {
+      Serial.println("WiFi failed to connect, time not synced");
+    }
+  }
+  else {
+    WITH_LOCK(Serial) {
+      Serial.println("WiFi connected, syncing time");
+    }
+    WITH_LOCK(Serial) {
+      http.get(timerequest, timeresponse, timeheaders);
+    }
+    WITH_LOCK(Serial) {
+      Serial.println("Time request returned: " + timeresponse.body);
+      Time.setTime(timeresponse.body.toInt());
+    }
+  }
 }
 
 void reportData(String payload) {
--- src/systemSync.hpp
+++ src/systemSync.hpp
@@ -1,39 +1,42 @@
 #pragma once
 #include "globalVariables.hpp"
 #include "constants.hpp"
+#include "http.hpp"
 
 void syncSystemTime() {
-  int WiFiConnectCountdown = kWiFiConnectionTimeout;
+  // int WiFiConnectCountdown = kWiFiConnectionTimeout;
 
-  WiFi.on();
-  WiFi.connect();
-  //wait for WiFi to connect for kWiFiConnectionTimeout
-  while(!WiFi.ready() && WiFiConnectCountdown != 0) {
-    WiFiConnectCountdown = WiFiConnectCountdown - kWiFiCheckInterval;
-    delay(kWiFiCheckInterval);
-    WITH_LOCK(Serial) {
-      Serial.println(WiFiConnectCountdown);
-    }
-  }
-  if(WiFi.ready()) {
-    WITH_LOCK(Serial){
-      Serial.println("WiFi connected, syncing time");
-    }
-    Particle.connect();
-    while(!Particle.connected()) {} // wait forever until cloud connects
-    Particle.syncTime(); // is async
-    while(Particle.syncTimePending()) { // wait for syncTime to complete
-      Particle.process();
-    }
-    WITH_LOCK(Serial){
-      Serial.printlnf("Current time is: %s", Time.timeStr().c_str());
-    }
-  }
-  else {
-    WITH_LOCK(Serial) {
-      Serial.println("WiFi failed to connect, skipping time synchronization");
-    }
-  }
+  // WiFi.on();
+  // WiFi.connect();
+  // //wait for WiFi to connect for kWiFiConnectionTimeout
+  // while(!WiFi.ready() && WiFiConnectCountdown != 0) {
+  //   WiFiConnectCountdown = WiFiConnectCountdown - kWiFiCheckInterval;
+  //   delay(kWiFiCheckInterval);
+  //   WITH_LOCK(Serial) {
+  //     Serial.println(WiFiConnectCountdown);
+  //   }
+  // }
+  // if(WiFi.ready()) {
+  //   WITH_LOCK(Serial){
+  //     Serial.println("WiFi connected, syncing time");
+  //   }
+  //   Particle.connect();
+  //   while(!Particle.connected()) {} // wait forever until cloud connects
+  //   Particle.syncTime(); // is async
+  //   while(Particle.syncTimePending()) { // wait for syncTime to complete
+  //     Particle.process();
+  //   }
+  //   WITH_LOCK(Serial){
+  //     Serial.printlnf("Current time is: %s", Time.timeStr().c_str());
+  //   }
+  // }
+  // else {
+  //   WITH_LOCK(Serial) {
+  //     Serial.println("WiFi failed to connect, skipping time synchronization");
+  //   }
+  // }
 
-  WiFi.off();
+  // WiFi.off();
+
+  getTime();
 }
